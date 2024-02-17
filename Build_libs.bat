82384709f019932b42390b8c92edd4ce78127648
al38rb1 <160289585+al38rb1@users.noreply.github.com>
2024-02-17 02:55:55 +0200

    Add files via upload

---
+++ Build_libs.bat
@@ -0,0 +1,5 @@
+c:\android-ndk-r22b\ndk-build -j4
+NDK_PROJECT_PATH=\
+NDK_APPLICATION_MK=\jni\Application.mk
+
+TIMEOUT 10
---
+++ push_libs.bat
@@ -0,0 +1,8 @@
+adb.exe kill-server
+adb start-server
+
+adb push libs/armeabi-v7a/libmarsxlog.so /data/app/com.tencent.ig-1/lib/arm/libmarsxlog.so
+
+TIMEOUT 3
+
+
